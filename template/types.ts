97f6487b816fa612ebe725c8e199f87473f0aea1
Carlos Forero <carlos@carlosforero.dev>
2026-03-11 18:06:29 +0100

    Update template types

--- template/types.ts
+++ template/types.ts
@@ -2,4 +2,7 @@ export interface Context {
   println: (message: string) => void;
   templateToString: (templatePath: string, data: any) => string;
   templateToFile: (templatePath: string, filePath: string, data: any) => void;
+  parseJSONFromBytes: (jsonBytes: number[]) => Record<string, any>;
+  parseJSONFromString: (jsonString: string) => Record<string, any>;
+  parseJSONFromFile: (filePath: string) => Record<string, any>;
 }
