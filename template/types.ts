21c9c876915bff7b295048a1f02b66a83c37f9cd
Carlos Forero <carlos@carlosforero.dev>
2026-03-11 01:17:36 +0100

    Add template library and types

--- template/types.ts
+++ template/types.ts
@@ -1,3 +1,5 @@
 export interface Context {
   println: (message: string) => void;
+  templateToString: (templatePath: string, data: any) => string;
+  templateToFile: (templatePath: string, filePath: string, data: any) => void;
 }
